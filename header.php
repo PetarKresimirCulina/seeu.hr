dcec2c61ae1315bd19ab96cc1d88b30a429062c9
Petar-Kresimir Culina <petar@taptoscan.com>
2018-01-30 15:22:48 +0100

    Google Analytics integration

--- header.php
+++ header.php
@@ -23,4 +23,15 @@
 	
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<?php wp_head(); ?>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113294131-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-113294131-1');
+</script>
+
   </head>
